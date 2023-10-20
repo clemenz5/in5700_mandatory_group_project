8334044da415b9d9d2b208592c2803a1687f9cdc
Lorenz Keefer <clemenzkafer@gmail.com>
2023-10-20 16:14:22 +0200

    added labels to edge server

--- edge_server/Cloud.cc
+++ edge_server/Cloud.cc
@@ -6,12 +6,33 @@ using namespace omnetpp;
 
 class Cloud: public cSimpleModule {
 private:
+    char displayString[20];
+    int numSentSmart;
+    int numSentComp;
+    int numReceivedSmart;
+    int numReceivedComp;
+    int sendMessagePower[2];
+    int receiveMessagePower[2];
+    int messageDelay;
+
+    cLabelFigure* total_num_cloud;
+    cLabelFigure* total_power_cloud_to_smart;
+    cLabelFigure* total_power_cloud_to_comp;
+    cLabelFigure* total_power_rcvd_cloud_to_smart;
+    cLabelFigure* total_power_rcvd_cloud_to_comp;
+    cLabelFigure* total_delay_cloud_to_smart;
+    cLabelFigure* total_delay_cloud_to_comp;
+    cLabelFigure* total_delay_rcvd_cloud_to_smart;
+    cLabelFigure* total_delay_rcvd_cloud_to_comp;
+
 
 protected:
     // The following redefined virtual function holds the algorithm.
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
     virtual void finish() override;
+    virtual void send(cMessage *msg, const char *gatename, int gateindex=-1) override;
+    void updateLabels();
 
 };
 
@@ -19,14 +40,91 @@ protected:
 Define_Module(Cloud);
 
 void Cloud::initialize() {
+    numSentComp = 0;
+    numSentSmart = 0;
+    numReceivedComp = 0;
+    numReceivedSmart = 0;
+
+    sendMessagePower[0] = 200;
+    sendMessagePower[1] = 100;
+
+    receiveMessagePower[0] = 300;
+    receiveMessagePower[1] = 300;
+
+    messageDelay = 100;
+
+    cCanvas* canvas = this->getParentModule()->getCanvas();
+
+    total_num_cloud= (cLabelFigure*)(canvas->getFigure("total_num_cloud"));
+    total_power_cloud_to_smart= (cLabelFigure*)(canvas->getFigure("total_power_cloud_to_smart"));
+    total_power_cloud_to_comp= (cLabelFigure*)(canvas->getFigure("total_power_cloud_to_comp"));
+    total_power_rcvd_cloud_to_smart= (cLabelFigure*)(canvas->getFigure("total_power_rcvd_cloud_to_smart"));
+    total_power_rcvd_cloud_to_comp= (cLabelFigure*)(canvas->getFigure("total_power_rcvd_cloud_to_comp"));
+
+    total_delay_cloud_to_smart= (cLabelFigure*)(canvas->getFigure("total_delay_cloud_to_smart"));
+    total_delay_cloud_to_comp= (cLabelFigure*)(canvas->getFigure("total_delay_cloud_to_comp"));
+    total_delay_rcvd_cloud_to_smart= (cLabelFigure*)(canvas->getFigure("total_delay_rcvd_cloud_to_smart"));
+    total_delay_rcvd_cloud_to_comp= (cLabelFigure*)(canvas->getFigure("total_delay_rcvd_cloud_to_comp"));
+    updateLabels();
 
 }
 
 void Cloud::handleMessage(cMessage *msg) {
+    if (!msg->isSelfMessage()) {
+        
+        if(msg->getArrivalGateId()==1048576){
+            numReceivedComp++;
+        } else {
+            numReceivedSmart++;
+        }
+        updateLabels();
+    }
     if (strcmp(msg->getName(), "1- Contents of Book Table") == 0) {
             send(new ComputerMsg("2- ACK"), "gate$o", 0);
         }
+    else if (strcmp(msg->getName(), "14- Book table index") == 0){
+            send(new ComputerMsg("15- ACK"), "gate$o", 0);
+    }
 
+}
+
+void Cloud::updateLabels(){
+    sprintf(displayString, "sent:%d rcvd:%d", numSentComp+numSentSmart, numReceivedComp+numReceivedSmart);
+    this->getDisplayString().setTagArg("t", 0,
+                displayString);
+
+    char temp[100];
+    sprintf(temp, "Total number of messages sent/received by the cloud= %d", numSentComp+numSentSmart+numReceivedComp+numReceivedSmart);
+    total_num_cloud->setText(temp);
+    sprintf(temp, "cloud (from cloud to smartphone)= %d", numSentSmart*sendMessagePower[0]);
+    total_power_cloud_to_smart->setText(temp);
+    sprintf(temp, "cloud (from cloud to comp)= %d", numSentComp*sendMessagePower[1]);
+    total_power_cloud_to_comp->setText(temp);
+
+    sprintf(temp, "cloud (from cloud to smartphone)= %d", numReceivedSmart*receiveMessagePower[0]);
+    total_power_rcvd_cloud_to_smart->setText(temp);
+    sprintf(temp, "cloud (from cloud to comp)= %d", numReceivedComp*receiveMessagePower[1]);
+    total_power_rcvd_cloud_to_comp->setText(temp);
+
+    sprintf(temp, "cloud (from cloud to smartphone)= %d", numReceivedSmart*messageDelay);
+    total_delay_cloud_to_smart->setText(temp);
+    sprintf(temp, "cloud (from cloud to comp)= %d", numReceivedComp*messageDelay);
+    total_delay_cloud_to_comp->setText(temp);
+
+    sprintf(temp, "cloud (from cloud to smartphone)= %d", numReceivedSmart*messageDelay);
+    total_delay_rcvd_cloud_to_smart->setText(temp);
+    sprintf(temp, "cloud (from cloud to comp)= %d", numReceivedComp*messageDelay);
+    total_delay_rcvd_cloud_to_comp->setText(temp);
+}
+
+void Cloud::send(cMessage *msg, const char *gatename, int gateindex){
+    cSimpleModule::send(msg, gatename, gateindex);
+    if(gateindex==0){
+        numSentComp++;
+    } else {
+        numSentSmart++;
+    }
+    updateLabels();
 }
 
 void Cloud::finish() {
--- edge_server/Host.cc
+++ edge_server/Host.cc
@@ -5,13 +5,33 @@ using namespace omnetpp;
 
 class Host: public cSimpleModule {
 private:
-    int drop_count;
+    int numDropped;
+    bool left;
+    char displayString[20];
+    int numSentCloud;
+    int numSentComp;
+    int numReceivedCloud;
+    int numReceivedComp;
+    int sendMessagePower[2];
+    int receiveMessagePower[2];
+    int messageDelay;
+    cLabelFigure* total_num_smartphone;
+    cLabelFigure* total_power_smart_to_cloud;
+    cLabelFigure* total_power_smart_to_comp;
+    cLabelFigure* total_power_rcvd_smart_to_cloud;
+    cLabelFigure* total_power_rcvd_smart_to_comp;
+    cLabelFigure* total_delay_smart_to_cloud;
+    cLabelFigure* total_delay_smart_to_comp;
+    cLabelFigure* total_delay_rcvd_smart_to_cloud;
+    cLabelFigure* total_delay_rcvd_smart_to_comp;
 
 protected:
     // The following redefined virtual function holds the algorithm.
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
     virtual void finish() override;
+    virtual void send(cMessage *msg, const char *gatename, int gateindex=-1) override;
+    void updateLabels();
 
 };
 
@@ -19,25 +39,64 @@ protected:
 Define_Module(Host);
 
 void Host::initialize() {
-    drop_count = 5;
+    numDropped = 0;
+    numSentComp = 0;
+    numSentCloud = 0;
+    numReceivedComp = 0;
+    numReceivedCloud = 0;
+
+    WATCH(numReceivedCloud);
+    WATCH(numReceivedComp);
+    WATCH(numDropped);
+
+    sendMessagePower[0] = 300;
+    sendMessagePower[1] = 300;
+
+    receiveMessagePower[0] = 250;
+    receiveMessagePower[1] = 250;
+
+    messageDelay= 100;
+
+    left = this->getParentModule()->par("left").boolValue();
     scheduleAt(simTime() + 38.0, new cMessage("browseBook"));
     scheduleAt(simTime() + 57.0, new cMessage("payBook"));
+
+    cCanvas* canvas = this->getParentModule()->getParentModule()->getCanvas();
+    total_num_smartphone = (cLabelFigure*)(canvas->getFigure("total_num_smartphone"));
+
+    total_power_smart_to_cloud= (cLabelFigure*)(canvas->getFigure("total_power_smart_to_cloud"));
+    total_power_smart_to_comp= (cLabelFigure*)(canvas->getFigure("total_power_smart_to_comp"));
+    total_power_rcvd_smart_to_cloud= (cLabelFigure*)(canvas->getFigure("total_power_rcvd_smart_to_cloud"));
+    total_power_rcvd_smart_to_comp= (cLabelFigure*)(canvas->getFigure("total_power_rcvd_smart_to_comp"));
+
+    total_delay_smart_to_cloud= (cLabelFigure*)(canvas->getFigure("total_delay_smart_to_cloud"));
+    total_delay_smart_to_comp= (cLabelFigure*)(canvas->getFigure("total_delay_smart_to_comp"));
+    total_delay_rcvd_smart_to_cloud= (cLabelFigure*)(canvas->getFigure("total_delay_rcvd_smart_to_cloud"));
+    total_delay_rcvd_smart_to_comp= (cLabelFigure*)(canvas->getFigure("total_delay_rcvd_smart_to_comp"));
+    updateLabels();
 }
 
 void Host::handleMessage(cMessage *msg) {
+    if (!msg->isSelfMessage()) {
+        if(msg->getArrivalGateId()==1048576){
+            numReceivedComp++;
+        } else {
+            numReceivedCloud++;
+        }
+    }
     if (strcmp(msg->getName(), "3- Computer ready to start") == 0
-            && drop_count > 0) {
+            && numDropped < 5) {
         this->getParentModule()->bubble("Message Lost");
-        drop_count--;
+        numDropped++;
     } else if (strcmp(msg->getName(), "3- Computer ready to start") == 0
-            && drop_count <= 0) {
+            && numDropped >= 5) {
         send(new ComputerMsg("4- ACK"), "gate$o", 0);
         send(new ComputerMsg("5- Where is the book I am looking for?"),
                 "gate$o", 0);
     } else if (strcmp(msg->getName(),
-            "7- The book you are looking for is in the right-hand shelf") == 0
+            "7- The book you are looking for is in the left-hand shelf") == 0
             || strcmp(msg->getName(),
-                    "8- The book you are looking for is in the left-hand shelf")
+                    "8- The book you are looking for is in the right-hand shelf")
                     == 0) {
         send(new ComputerMsg("9- ACK"), "gate$o", 0);
     } else if (strcmp(msg->getName(), "browseBook") == 0) {
@@ -47,6 +106,46 @@ void Host::handleMessage(cMessage *msg) {
     } else if (strcmp(msg->getName(), "12- Book payed") == 0) {
         send(new ComputerMsg("13- ACK"), "gate$o", 0);
     }
+    updateLabels();
+}
+
+void Host::updateLabels(){
+    sprintf(displayString, "sent:%d rcvd:%d lost:%d", numSentComp+numSentCloud, numReceivedComp+numReceivedCloud-numDropped, numDropped);
+    this->getParentModule()->getDisplayString().setTagArg("t", 0,
+            displayString);
+
+    char temp[80];
+    sprintf(temp, "Total number of messages sent/received by the smartphone= %d", numSentComp+numSentCloud+numReceivedComp+numReceivedCloud-numDropped);
+    total_num_smartphone->setText(temp);
+    sprintf(temp, "smartphone (from smartphone to cloud)= %d", numSentCloud*sendMessagePower[0]);
+    total_power_smart_to_cloud->setText(temp);
+    sprintf(temp, "smartphone (from smartphone to comp)= %d", numSentComp*sendMessagePower[1]);
+    total_power_smart_to_comp->setText(temp);
+
+    sprintf(temp, "smartphone (from smartphone to cloud)= %d", numReceivedCloud*receiveMessagePower[0]);
+    total_power_rcvd_smart_to_cloud->setText(temp);
+    sprintf(temp, "smartphone (from smartphone to comp)= %d", (numReceivedComp-numDropped)*receiveMessagePower[1]);
+    total_power_rcvd_smart_to_comp->setText(temp);
+
+    sprintf(temp, "smartphone (from smartphone to cloud)= %d", numReceivedCloud*messageDelay);
+    total_delay_smart_to_cloud->setText(temp);
+    sprintf(temp, "smartphone (from smartphone to comp)= %d", (numReceivedComp-numDropped)*messageDelay);
+    total_delay_smart_to_comp->setText(temp);
+
+    sprintf(temp, "smartphone (from smartphone to cloud)= %d", numReceivedCloud*messageDelay);
+    total_delay_rcvd_smart_to_cloud->setText(temp);
+    sprintf(temp, "smartphone (from smartphone to comp)= %d", (numReceivedComp-numDropped)*messageDelay);
+    total_delay_rcvd_smart_to_comp->setText(temp);
+}
+
+void Host::send(cMessage *msg, const char *gatename, int gateindex){
+    cSimpleModule::send(msg, gatename, gateindex);
+    if(gateindex==0){
+        numSentComp++;
+    } else {
+        numSentCloud++;
+    }
+    updateLabels();
 }
 
 void Host::finish() {
